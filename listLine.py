427ccd26373aa9892d8ef1ec525d20aa9ed1fa12
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-06-05 14:56:28 +0700

    Add meaning in iterdir

--- listLine.py
+++ listLine.py
@@ -5,5 +5,6 @@ import sys
 path = sys.argv[1]
 currentDirectory = pathlib.Path(path)
 
+# .iterdir : list dir or file in here
 for currentFile in currentDirectory.iterdir():  
     print(currentFile)
